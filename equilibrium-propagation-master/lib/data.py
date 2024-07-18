a76e6d1376e0096fde3297fc9a1e1a532f97cccc
Chenyan Lu <88831699+Era5eMe@users.noreply.github.com>
2024-07-18 15:19:28 +0800

    updated validation loader

--- equilibrium-propagation-master/lib/data.py
+++ equilibrium-propagation-master/lib/data.py
@@ -58,6 +58,13 @@ def create_mnist_loaders(batch_size):
                                  target_transform=_one_hot_ten
                                  )
 
+    # Define the sizes of the training and validation sets
+    train_size = int(0.9 * len(mnist_train))
+    val_size = len(mnist_train) - train_size
+
+    # Split the dataset
+    mnist_train, mnist_val = torch.utils.data.random_split(mnist_train, [train_size, val_size])
+
     mnist_test = datasets.MNIST('../data/', train=False, download=True,
                                 transform=transforms.Compose([
                                     transforms.ToTensor(),
@@ -73,7 +80,10 @@ def create_mnist_loaders(batch_size):
     train_loader = torch.utils.data.DataLoader(
         mnist_train, batch_size=batch_size, drop_last=True, shuffle=True, **kwargs)
 
+    val_loader = torch.utils.data.DataLoader(
+        mnist_val, batch_size=batch_size, drop_last=True, shuffle=False, **kwargs)
+
     test_loader = torch.utils.data.DataLoader(
         mnist_test, batch_size=batch_size, drop_last=True, shuffle=False, **kwargs)
 
-    return train_loader, test_loader
+    return train_loader, val_loader, test_loader
